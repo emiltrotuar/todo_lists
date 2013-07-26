44f56b8794d234a25f25532e9d5a62997e7af1af
dimas <cartobus@gmail.com>
2013-07-26 18:13:15 +0300

    Keep working on different features

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,7 +1,9 @@
 $(function() {
-	var i = 0;
+	var tmp;	
 
 	$('.edit_task').submitOnCheck();
+
+	$('.draggable').draggable({ containment: "parent" });
 
 	$(document).on('keypress', '.ed_prj_inp', function(event) { /* change project name */
 		if (event.keyCode === 13) {
@@ -17,9 +19,11 @@ $(function() {
 	$(document).on('keypress', '.ed_task_inp', function(event) { /* change task name */
 		if (event.keyCode === 13) {
 			$(this).closest('.draggable').find('.task_input').focus();
+			$(this).closest('tr').find('td:nth-child(3) > img').after(tmp);
 			$(this).parent().submit();
 		} else if (event.keyCode === 27) {
 			$(this).closest('.draggable').find('.task_input').focus();
+			$(this).closest('tr').find('td:nth-child(3) > img').after(tmp);
 			$(this).parent().prev().show();
 			$(this).parent().remove();
 		}
@@ -31,26 +35,47 @@ $(function() {
 			$(this).closest('form').prev().show();
 			$(this).parent().remove();
 		}
+
 	});
 
 	$(document).on('click', '.del_prj', function(){  /* delete project */
 		$(this).closest('.draggable').fadeOut('slow', function() { this.closest('.draggable').remove(); });
 	});
 
+	$(document).on('click', '.ed_task', function() { /* delete task */
+		tmp = $(this).detach(); 
+	});
+
 	$(document).on('click', '.del_task', function() { /* delete task */
 		$(this).closest('tr').fadeOut('slow', function() { $(this).closest('tr').remove(); }); 
 	});
-	
-	$(document).on('mouseover', '.draggable', function(){
-		$(this).draggable({ containment: "parent" });
-		$(this).find('tbody').sortable();
+
+	$(document).on('mouseenter', '.reorder', function(){
+		$(this).closest('tbody').sortable({
+			axis: "y",
+			revert: 200,
+			opacity: 0.5,
+			containment: "parent",
+			sort: function(event, ui) {
+				ui.item.find('td:nth-child(2)').width(ui.item.siblings('tr').find('td:nth-child(2)').width());
+			},
+			stop: function(event, ui) { 
+				ui.item.find('td:nth-child(3)').find('*').hide();
+			}
+		});
 	});
 
-	$(document).on('mouseover', 'tr', function(){
-		$(this).find('td:nth-child(3)').find()
+	$(document).on('mousedown', 'tbody td:nth-child(1), tbody td:nth-child(2)', function(){
+		$(this).closest('tbody').sortable('destroy');
 	});
+
+	$(document).on('mouseenter', 'tr', function(){
+		$(this).find('td:nth-child(3)').find('*').show();
+	}).on('mouseleave', 'tr', function(){
+		$(this).find('td:nth-child(3)').find('*').hide();
+	});
+
 });
-
 
 jQuery.fn.submitOnCheck = function(){
 	this.find('input[type=checkbox]').click(function() {
