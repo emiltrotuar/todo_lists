6cef6825c0767f17e8f3e6c8b8bef2eac9ce5926
dimas <cartobus@gmail.com>
2015-02-21 21:10:23 +0200

    changed task content max length

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
   attr_accessible :content, :complete, :date, :position
   belongs_to :project
   validates :project_id, presence: true
-  validates :content, presence: true, length: { maximum: 100 }
+  validates :content, presence: true, length: { maximum: 350 }
   default_scope order: 'tasks.position ASC'
   acts_as_list scope: :project
 end
